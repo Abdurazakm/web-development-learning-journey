11ab9240f06514b44f182fb934a7172441f95c29
abdurazakm <abdulkem5472gmail.com>
2025-06-26 23:36:29 +0300

    emoji dictionary with arrow function completed

--- React/emoji_dict/src/App.jsx
+++ React/emoji_dict/src/App.jsx
@@ -2,26 +2,24 @@ import React from "react";
 import Entry from "./component/Entry";
 import emojiDictionary from "./emojiDictionary";
 
-function createEmoji(emojiDi) {
-  return (
-    <Entry
-      key={emojiDi.id}
-      emoji={emojiDi.emoji}
-      name={emojiDi.name}
-      meaning={emojiDi.meaning}
-    />
-  );
-}
-
-function App() {
+const App = () => {
   return (
     <div className="container">
-      <h1><span className="heading">emojipedia</span></h1>
+      <h1>
+        <span className="heading">emojipedia</span>
+      </h1>
       <dl className="dictionary">
-        {emojiDictionary.map(createEmoji)}
+        {emojiDictionary.map((emojiDi) => (
+          <Entry
+            key={emojiDi.id}
+            emoji={emojiDi.emoji}
+            name={emojiDi.name}
+            meaning={emojiDi.meaning}
+          />
+        ))}
       </dl>
     </div>
   );
-}
+};
 
 export default App;
